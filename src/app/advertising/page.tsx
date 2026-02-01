6fb931f9456f516a320c83b343d3343d221f3c61
Yash-srivastav16 <yashsrivastav8115@gmail.com>
2026-02-01 12:56:27 +0530

    Build issue fix

---
+++ src/app/advertising/page.tsx
@@ -0,0 +1,7 @@
+export default function Advertising() {
+  return (
+    <div>
+      {/* Your UI goes here */}
+    </div>
+  )
+}
---
+++ src/app/soft-toys/page.tsx
@@ -0,0 +1,7 @@
+export default function SoftToys() {
+  return (
+    <div>
+      {/* Your UI goes here */}
+    </div>
+  )
+}
